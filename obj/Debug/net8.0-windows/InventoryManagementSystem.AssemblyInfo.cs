f6db7ad48f60e863dd430f29b7546468ea61b8c1
Dork Meas <dorkmeas@gmail.com>
2026-09-02 23:49:21 +0700

    Inventory Management System

--- obj/Debug/net8.0-windows/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+942533234c77ee376077aaa9512b9d33125b4e65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abea35170d786ea7668350c3a67fb9022aabf704")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
